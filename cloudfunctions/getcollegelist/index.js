0e4066ac550da9bc59156fb949563e3c1ac690b6
hrmzone <78049500@qq.com>
2020-02-15 19:57:11 +0800

    page index基本功能、样式完成

---
+++ cloudfunctions/getcollegelist/index.js
@@ -0,0 +1,16 @@
+const cloud = require('wx-server-sdk')
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV
+})
+
+exports.main = async (event) => {
+  const { ENV, OPENID, APPID } = cloud.getWXContext()
+
+  // 如果云函数所在环境为 abc，则下面的调用就会请求到 abc 环境的数据库
+  const dbResult = await cloud.database().collection('collegelist').orderBy('cname','desc').get()
+
+  return {
+    dbResult
+  }
+
+}
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -5,14 +5,76 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    value:'',
+    collegelist:[],
+    num:null,
+    msg:''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    //获取全部collegelist
+    wx.showLoading({
+      title: '加载中....',
+    });
+    wx.cloud.callFunction({
+      name:'getcollegelist'
+    }).then(
+      res=>{
+        console.log("onLoad():1:",res.result.dbResult.data.length)
+        this.setData({
+          collegelist:res.result.dbResult.data,
+          num: res.result.dbResult.data.length
+        })
+        wx.hideLoading()
+      }
+    );
 
+    //获取公告消息msg1
+    const db=wx.cloud.database();
+    db.collection("msg").where({
+      id:1
+    }).get().then(
+      res=>{
+        console.log("onLoad():2:", res.data[0].content)
+        this.setData({
+          msg:res.data[0].content
+        })
+      }
+    )
+  },
+
+  onChange(e) {
+    this.setData({
+      value:e.detail
+    });
+  },
+
+  onSearch() {
+    console.log("onSearch():",this.data.value);
+    wx.cloud.callFunction({
+      name:'getcollege',
+      data:{
+        value:this.data.value
+      }
+    }).then(
+      res=>{
+        console.log("onSearch():2:",res.result.dbResult.data.length);
+        this.setData({
+          collegelist: res.result.dbResult.data,
+          num: res.result.dbResult.data.length
+        })
+      }
+    )
+  },
+
+  onCancel() {
+    this.setData({
+      value:''
+    });
+    console.log("onCancel():",this.data.value);
   },
 
   /**
