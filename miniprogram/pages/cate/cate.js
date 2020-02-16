c9977b1016964004f8357a74bb67b611d625e724
hrmzone <78049500@qq.com>
2020-02-16 22:37:29 +0800

    page cate:大类页面功能及UI可以正常使用。

--- miniprogram/pages/cate/cate.js
+++ miniprogram/pages/cate/cate.js
@@ -5,6 +5,10 @@ Page({
    * 页面的初始数据
    */
   data: {
+    scate:'一类',
+    catelist:[],
+    num:0,
+    msg:''
 
   },
 
@@ -12,7 +16,37 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.showLoading({
+      title: '加载中...',
+    })
+    this.setData({
+      scate:options.scate
+    })
+    const db = wx.cloud.database();
+    db.collection("msg").where({
+      id: 4
+    }).get().then(
+      res => {
+        this.setData({
+          msg: res.data[0].content
+        })
+      }
+    )
+    wx.cloud.callFunction({
+      name:'getcatelist',
+      data:{
+        scate:this.data.scate
+      }
+    }).then(
+      res=>{
+        wx.hideLoading();
+        console.log("onLoad():1:",res.result.list.data);
+        this.setData({
+          catelist: res.result.list.data,
+          num: res.result.list.data.length
+        })
+      }
+    )
   },
 
   /**
