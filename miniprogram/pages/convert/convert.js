29aa4dd42a0c5b4951fc4178acfbce94743526e5
hrmzone <78049500@qq.com>
2021-03-06 15:03:04 +0800

    页面学考成绩折算计算机全部完成，并添加至主页

--- miniprogram/pages/convert/convert.js
+++ miniprogram/pages/convert/convert.js
@@ -34,31 +34,32 @@ Page({
     titleValue2:2,
     titleValue3:3,
     titleValue4:4,
-    gradeValue1:2,
-    gradeValue2:2,
+    gradeValue1:0,
+    gradeValue2:0,
     gradeValue3:0,
-    gradeValue4:2,
+    gradeValue4:0,
     score:0,
     course:'物理',
-    college:'邢台职业技术学院',
-    pro:'建设工程管理',
+    college:'邢台职业技术学院、石家庄理工职业学院',
+    pro:'建设工程管理、建筑设计',
     label1:'',
     label2:'',
     msg:'',
-    yscore:0,
-    sscore:0,
-    zscore:0,
+    yscore:130,
+    sscore:130,
+    zscore:90,
 
   },
 
   /**
    * 生命周期函数--监听页面加载
+   * 获取转换表数据以及msg第9条广告通知
    */
   onLoad: function (options) {
     const db = wx.cloud.database();
     db.collection("convert").orderBy("id","asc").get().then(
       res=> {
-        console.log(res.data[0].cate)
+        // console.log(res.data[0].cate)
         this.setData({
           convertmap:res.data
         })
@@ -76,25 +77,106 @@ Page({
       }
     )
   },
-
+/**
+ * 根据学考等级，计算语文成绩
+ * @param {*} e 
+ */
   onYChange(e) {
     console.log(e.detail)
-    console.log(this.data.convertmap[10])
+    // console.log(this.data.convertmap[10])
+    let that=this
+    // console.log(this.gradeConvert(this.data.convertmap[0],e.detail))
     this.setData({
-      yscore:e.detail
+      yscore:that.gradeConvert(this.data.convertmap[10],e.detail)
+    })
+    this.calc()
+  },
+
+  /**
+   * 根据学考等级，计算数学成绩
+   * @param {*} e 
+   */
+  onSChange(e) {
+    console.log(e.detail)
+    let that=this
+    this.setData({
+      sscore:that.gradeConvert(this.data.convertmap[11],e.detail)
+    })
+    this.calc()
+  },
+
+  /**
+   * 根据单招大类和学考等级，计算专业基础知识成绩
+   * @param {*} e 
+   */
+  onZChange(e) {
+    console.log(e.detail)
+    // console.log(this.data.convertmap[10])
+    let that=this
+    // console.log(this.gradeConvert(this.data.convertmap[0],e.detail))
+    this.setData({
+      zscore:that.gradeConvert(this.data.convertmap[this.data.gradeValue3],e.detail)
+    })
+    this.calc()
+  },
+
+    /**
+   * 根据不同考试科目和序号，将等级转换为相应的分数
+   * 0->A...3->D
+   */
+  gradeConvert:function(classCate,grade) {
+    if(grade==0) {
+      // console.log(classCate.A)
+      return classCate.A
+    } else if(grade==1) {
+      return classCate.B
+    } else if(grade==2) {
+      return classCate.C
+    } else if(grade==3) {
+      return classCate.D
+    } else {
+      return 0
+    }
+  },
+
+/**
+ * 职业技能单招大类选择触发动作函数，将大类信息（考试科目、院校等）填充至变量
+ * @param {*} e 
+ */
+  onCChange(e) {
+    console.log(e.detail)
+    let that=this
+    this.cateConvert(that.data.convertmap[e.detail])
+    this.calc()
+  },
+
+/**
+ * 
+ * 根据所选单招考试大类，讲大类考试科目、推荐院校、专业以及label填充相应的值
+ * 用于被onCChange方法调用
+ */
+  cateConvert:function(classCate) {
+    console.log(classCate.course,classCate.cate)
+    this.setData({
+      course:classCate.course,
+      college:classCate.college,
+      pro:classCate.pro,
+      label1:classCate.label1,
+      label2:classCate.label2,
+      gradeValue3:classCate.id
     })
   },
-  
-  onCChange(e) {
 
-  },
+
 
   /**
    * 计算三个科目总分
    */
   calc:function() {
+    let that=this
+    console.log(that.data.yscore,that.data.sscore,that.data.zscore)
     this.setData({
-      score:yscore+sscore+zscore
+      score:that.data.yscore+that.data.sscore+that.data.zscore
     })
   },
 
